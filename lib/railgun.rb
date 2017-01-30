af9aa92b1bd8ba7a9310530503401079bd02af22
Sean Johnson <pirogoeth@users.noreply.github.com>
2017-01-30 14:35:46 -0600

    Implement Railgun - Rails ActiveMailer interface for Mailgun (#72)

---
+++ lib/railgun.rb
@@ -0,0 +1,10 @@
+require 'action_mailer'
+require 'active_support/rescuable'
+
+require 'railgun/attachment'
+require 'railgun/errors'
+require 'railgun/mailer'
+require 'railgun/message'
+
+module Railgun
+end
