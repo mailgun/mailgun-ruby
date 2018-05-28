fdea30a2ed211a7cc9fd8d716665e0744e9dd94a
Kasper Grubbe <kawsper@gmail.com>
2018-05-28 19:00:59 +0100

    path doesn’t return URL so call always return nil

--- spec/unit/events/events_spec.rb
+++ spec/unit/events/events_spec.rb
@@ -11,6 +11,25 @@ describe 'The method get' do
   end
 end
 
+describe 'Pagination' do
+  it 'should return a proper hash of log data.' do
+    @mg_obj = Mailgun::UnitClient.new('events')
+    events = Mailgun::Events.new(@mg_obj, "samples.mailgun.org")
+    result = events.get()
+
+    json = JSON.parse(result.body)
+    expect(json).to include("paging")
+    expect(json["paging"]).to include("next")
+    expect(json["paging"]).to include{"previous"}
+  end
+
+  it 'should calculate proper next-page url' do
+    events = Mailgun::Events.new(@mg_obj, "samples.mailgun.org")
+    output = events.send(:extract_endpoint_from, '/v3/samples.mailgun.org/events/W3siYiI6ICIyMDE3LTA1LTEwVDIwOjA2OjU0LjU3NiswMDowMCIsICJlIjogIjIwMTctMDUtMDhUMjA6MDY6NTQuNTc3KzAwOjAwIn0sIHsiYiI6ICIyMDE3LTA1LTEwVDIwOjA2OjU0LjU3NiswMDowMCIsICJlIjogIjIwMTctMDUtMDhUMjA6MDY6NTQuNTc3KzAwOjAwIn0sIFsiZiJdLCBudWxsLCBbWyJhY2NvdW50LmlkIiwgIjU4MDUyMTg2NzhmYTE2MTNjNzkwYjUwZiJdLCBbImRvbWFpbi5uYW1lIiwgInNhbmRib3gyOTcwMTUyYWYzZDM0NTU5YmZjN2U3MTcwM2E2Y2YyNC5tYWlsZ3VuLm9yZyJdXSwgMTAwLCBudWxsXQ==')
+
+    expect(output).to eq 'W3siYiI6ICIyMDE3LTA1LTEwVDIwOjA2OjU0LjU3NiswMDowMCIsICJlIjogIjIwMTctMDUtMDhUMjA6MDY6NTQuNTc3KzAwOjAwIn0sIHsiYiI6ICIyMDE3LTA1LTEwVDIwOjA2OjU0LjU3NiswMDowMCIsICJlIjogIjIwMTctMDUtMDhUMjA6MDY6NTQuNTc3KzAwOjAwIn0sIFsiZiJdLCBudWxsLCBbWyJhY2NvdW50LmlkIiwgIjU4MDUyMTg2NzhmYTE2MTNjNzkwYjUwZiJdLCBbImRvbWFpbi5uYW1lIiwgInNhbmRib3gyOTcwMTUyYWYzZDM0NTU5YmZjN2U3MTcwM2E2Y2YyNC5tYWlsZ3VuLm9yZyJdXSwgMTAwLCBudWxsXQ=='
+  end
+end
 
 describe 'The method next' do
   it 'should return the next series of data.' do
