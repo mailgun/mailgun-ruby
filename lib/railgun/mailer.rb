0f538d0d172a1b1f9b6c681ee1c28bb8857b90f4
Sean Johnson <sean@mailgun.com>
2019-01-24 16:48:52 -0600

    Ignore `reply-to` set in headers

--- lib/railgun/mailer.rb
+++ lib/railgun/mailer.rb
@@ -11,7 +11,7 @@ module Railgun
   class Mailer
 
     # List of the headers that will be ignored when copying headers from `mail.header_fields`
-    IGNORED_HEADERS = %w[ to from subject ]
+    IGNORED_HEADERS = %w[ to from subject reply-to ]
 
     # [Hash] config ->
     #   Requires *at least* `api_key` and `domain` keys.
@@ -99,7 +99,11 @@ module Railgun
     mail.headers(mail.mailgun_headers || {})
     mail.header_fields.each do |field|
       header = field.name.downcase
-      msg_headers[header] = field.value
+      if msg_headers.include? header
+        msg_headers[header] = [msg_headers[header], field.value].flatten
+      else
+        msg_headers[header] = field.value
+      end
     end
 
     msg_headers.each do |k, v|
--- spec/unit/railgun/mailer_spec.rb
+++ spec/unit/railgun/mailer_spec.rb
@@ -163,7 +163,7 @@ describe 'Railgun::Mailer' do
     expect(ActionMailer::Base.deliveries).to include(message)
   end
 
-  it 'overwrites headers from `headers()` with `mailgun_headers`' do
+  it 'ignores `reply-to` in headers' do
     message = UnitTestMailer.plain_message('test@example.org', '', {
       'reply-to' => 'user@example.com',
     })
@@ -171,11 +171,12 @@ describe 'Railgun::Mailer' do
       'Reply-To' => 'administrator@example.org',
     }
     message.headers({'REPLY-TO' => 'admin@example.net'})
+    message.reply_to = "dude@example.com.au"
 
     body = Railgun.transform_for_mailgun(message)
     expect(body).to include('h:reply-to')
     expect(body).not_to include('h:Reply-To')
-    expect(body['h:reply-to']).to eq('administrator@example.org')
+    expect(body['h:reply-to']).to eq('dude@example.com.au')
   end
 
   it 'treats `headers()` names as case-insensitve' do
@@ -197,5 +198,20 @@ describe 'Railgun::Mailer' do
     body = Railgun.transform_for_mailgun(message)
     expect(body).to include('h:x-big-value')
     expect(body['h:x-big-value']).to eq("1")
+  end
+
+  it 'handles multi-value, mixed case headers correctly' do
+    message = UnitTestMailer.plain_message('test@example.org', '', {})
+    message.headers({
+      'x-neat-header' => 'foo',
+      'X-Neat-Header' => 'bar',
+      'X-NEAT-HEADER' => 'zoop',
+    })
+
+    body = Railgun.transform_for_mailgun(message)
+    expect(body).to include('h:x-neat-header')
+    expect(body['h:x-neat-header']).to include('foo')
+    expect(body['h:x-neat-header']).to include('bar')
+    expect(body['h:x-neat-header']).to include('zoop')
   end
 end
