49a7e29c8714bc14b2968898b410d587d6158540
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2019-02-23 01:12:18 +1100

    Don't force ActionMailer to lazy load before configuration.

--- lib/railgun/railtie.rb
+++ lib/railgun/railtie.rb
@@ -2,8 +2,11 @@ require 'railgun/mailer'
 
 module Railgun
   class Railtie < ::Rails::Railtie
-    config.before_configuration do
-      ActionMailer::Base.add_delivery_method :mailgun, Railgun::Mailer
+    initializer "railgun.configure_rails_initialization" do
+      ActiveSupport.on_load(:action_mailer) do
+        add_delivery_method :mailgun, Railgun::Mailer
+        ActiveSupport.run_load_hooks(:mailgun_mailer, Railgun::Mailer)
+      end
     end
   end
 end
