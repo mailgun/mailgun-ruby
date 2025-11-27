002b27f3035f8659996f9d47889d905cfa5ac3d5
Alex Lebedev <6421109+alex-leb@users.noreply.github.com>
2025-11-28 01:19:59 +0200

    DE-1618: Tags - Add missing endpoints and refactor existing (#369)
    * DE-1618: Tags - Add missing endpoints and refactor existing
    
    Signed-off-by: Alex Lebedev <6421109+alex-leb@users.noreply.github.com>
    
    * DE-1618: Update analytics_tags.rb
    
    Signed-off-by: Alex Lebedev <6421109+alex-leb@users.noreply.github.com>
    
    * DE-1618: Fix specs
    
    Signed-off-by: Alex Lebedev <6421109+alex-leb@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Alex Lebedev <6421109+alex-leb@users.noreply.github.com>

---
+++ lib/mailgun/tags/analytics_tags.rb
@@ -0,0 +1,35 @@
+require 'mailgun/exceptions/exceptions'
+require 'mailgun/helpers/api_version_checker'
+
+module Mailgun
+
+  # A Mailgun::AnalyticsTags object is a simple CRUD interface to Mailgun Tags.
+  # Uses Mailgun
+  class AnalyticsTags
+    include ApiVersionChecker
+
+    # Public: creates a new Mailgun::AnalyticsTags instance.
+    # Defaults to Mailgun::Client
+    def initialize(client = Mailgun::Client.new(Mailgun.api_key, Mailgun.api_host || 'api.mailgun.net', 'v1'))
+      @client = client
+    end
+
+    def update(tag, description)
+      @client.put('analytics/tags', { tag: tag, description: description }, body_params: true ).to_h['message'] == 'Tag updated'
+    end
+
+    def list(options = {})
+      @client.post('analytics/tags', options).to_h['items']
+    end
+
+    def remove(tag)
+      @client.delete('analytics/tags', { tag: tag }, body_params: true).to_h['message'] == 'Tag deleted'
+    end
+
+    def limits
+      @client.get('analytics/tags/limits').to_h
+    end
+
+    enforces_api_version 'v1', :update, :list, :remove, :limits
+  end
+end
--- lib/mailgun/tags/tags.rb
+++ lib/mailgun/tags/tags.rb
@@ -12,6 +12,9 @@ module Mailgun
       @client = client
     end
 
+    ### Deprecated Tags ###
+    # TODO: remove in v 1.5
+
     # Public: Get Tags
     #
     # domain - [String] Domain name where tag is stored
@@ -20,6 +23,7 @@ module Mailgun
     #
     # Returns [Array] A list of tags (hash)
     def get_tags(domain, options = {})
+      warn('This API is deprecated in favor of our new analytics Tags API')
       fail(ParameterError, 'No domain given to store template on', caller) unless domain
       @client.get("#{domain}/tags", options).to_h['items']
     end
@@ -31,6 +35,7 @@ module Mailgun
     #
     # Returns [Hash] Information on the requested tag.
     def get_tag(domain, tag)
+      warn('This API is deprecated in favor of our new analytics Tags API')
       fail(ParameterError, 'No domain given to find on Mailgun', caller) unless domain
       fail(ParameterError, 'No tag name given to find on provided domain', caller) unless tag
       @client.get("#{domain}/tags/#{tag}").to_h!
@@ -45,6 +50,7 @@ module Mailgun
     #
     # Returns [Boolean] if successful or not
     def update(domain, tag, options = {})
+      warn('This API is deprecated in favor of our new analytics Tags API')
       fail(ParameterError, 'No domain given to find on Mailgun', caller) unless domain
       fail(ParameterError, 'No tag name given to find on provided domain', caller) unless tag
       @client.put("#{domain}/tags/#{tag}", options).to_h['message'] == 'Tag updated'
@@ -63,6 +69,7 @@ module Mailgun
     #
     # Returns [Hash] of tag stats info
     def get_tag_stats(domain, tag, options = {})
+      warn('This API is deprecated in favor of our new analytics Tags API')
       fail(ParameterError, 'No domain given to find on Mailgun', caller) unless domain
       fail(ParameterError, 'No tag name given to find on provided domain', caller) unless tag
       @client.get("#{domain}/tags/#{tag}/stats", options).to_h
@@ -76,6 +83,7 @@ module Mailgun
     #
     # Returns [Boolean] if successful or not
     def remove(domain, tag)
+      warn('This API is deprecated in favor of our new analytics Tags API')
       fail(ParameterError, 'No domain given to remove on Mailgun', caller) unless domain
       fail(ParameterError, 'No template name given to find on provided domain', caller) unless tag
       @client.delete("#{domain}/tags/#{tag}").to_h['message'] == 'Tag deleted'
@@ -88,6 +96,7 @@ module Mailgun
     #
     # Returns [Hash] of countries of origin for a given domain
     def get_countries_aggregated_stats(domain, tag)
+      warn('This API is deprecated in favor of our new analytics Tags API')
       fail(ParameterError, 'No domain given to find on Mailgun', caller) unless domain
       fail(ParameterError, 'No tag name given to find on provided domain', caller) unless tag
       @client.get("#{domain}/tags/#{tag}/stats/aggregates/countries").to_h
@@ -100,6 +109,7 @@ module Mailgun
     #
     # Returns [Hash] of email providers for a given domain
     def get_providers_aggregated_stats(domain, tag)
+      warn('This API is deprecated in favor of our new analytics Tags API')
       fail(ParameterError, 'No domain given to find on Mailgun', caller) unless domain
       fail(ParameterError, 'No tag name given to find on provided domain', caller) unless tag
       @client.get("#{domain}/tags/#{tag}/stats/aggregates/providers").to_h
@@ -112,6 +122,7 @@ module Mailgun
     #
     # Returns [Hash] of devices for a given domain
     def get_devices_aggregated_stats(domain, tag)
+      warn('This API is deprecated in favor of our new analytics Tags API')
       fail(ParameterError, 'No domain given to find on Mailgun', caller) unless domain
       fail(ParameterError, 'No tag name given to find on provided domain', caller) unless tag
       @client.get("#{domain}/tags/#{tag}/stats/aggregates/devices").to_h
