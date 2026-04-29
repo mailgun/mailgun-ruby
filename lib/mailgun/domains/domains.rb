d11c7dada5c8b8672afbdec5fd003b063909990f
Alex Lebedev <6421109+alex-leb@users.noreply.github.com>
2026-04-29 15:13:22 +0300

    DE-1776: Domains - Extract Domain Keys
    Signed-off-by: Alex Lebedev <6421109+alex-leb@users.noreply.github.com>

--- lib/mailgun/domains/domains.rb
+++ lib/mailgun/domains/domains.rb
@@ -88,99 +88,6 @@ module Mailgun
 
     # ==== End of Core Domains methods ====
 
-    # ==== Domain::Keys methods ====
-
-    # Public: List keys for all domains
-    #
-
-    # options - [Hash] of
-    #   page - [String] Encoded paging information, provided via 'next', 'previous' links
-    #   limit - [Integer] Limits the number of items returned in a request
-    #   signing_domain  - [String] Filter by signing domain
-    #   selector - [String] Filter by selector
-    #
-    # Returns [Hash] with message key
-    def list_domain_keys(options = {})
-      @client.get('dkim/keys', options).to_h
-    end
-
-    # Public: Create a domain key
-    #
-    # options - [Hash] of
-    #   signing_domain  - [String] Name of the domain (ex. domain.com)
-    #   selector - [String] Selector to be used for the new domain key
-    #   bits - [Integer] Key size, can be 1024 or 2048
-    #   pem - [String] Private key PEM
-    #
-    # Returns [Hash] with message key
-    def create_domain_key(options = {})
-      @client.post('dkim/keys', options).to_h
-    end
-
-    # Public: Delete a domain key.
-    #
-    # options - [Hash] of
-    #   signing_domain - [Integer] Name of the domain (ex. domain.com)
-    #   selector - [String] Name of the selector
-    #
-    # Returns [Hash] with message key
-    def delete_domain_key(options = {})
-      @client.delete('dkim/keys', options).to_h
-    end
-
-    # Public: Activate a domain key for a specified authority and selector.
-    #
-    # domain    - [String] Name of the domain (ex. domain.com)
-    # selector  - [String] The selector you want to activate for the domain key
-    #
-    # Returns [Hash] with message key and autority + selector data
-    def activate_domain_key(domain, selector)
-      @client.put("domains/#{domain}/keys/#{selector}/activate", {}).to_h
-    end
-
-    # Public: Lists the domain keys for a specified signing domain / authority
-    #
-    # domain  - [String] Name of the domain (ex. domain.com)
-    #
-    # Returns [Hash] with domain keys data
-    def get_domain_keys(domain)
-      @client.get("domains/#{domain}/keys").to_h
-    end
-
-    # Public: Deactivate a domain key for a specified authority and selector
-    #
-    # domain    - [String] Name of the domain (ex. domain.com)
-    # selector  - [String] The selector you want to activate for the domain key
-    #
-    # Returns [Hash] with message key and autority + selector data
-    def deactivate_domain_key(domain, selector)
-      @client.put("domains/#{domain}/keys/#{selector}/deactivate", {}).to_h
-    end
-
-    # Public: Change the DKIM authority for a domain.
-    #
-    # domain  - [String] Name of the domain (ex. domain.com)
-    # options - [Hash] of
-    #   self  - [Boolean] true - the domain will be the DKIM authority for itself even
-    #           if the root domain is registered on the same mailgun account
-    #
-    # Returns [Hash] Information on the DKIM authority
-    def update_domain_dkim_authority(domain, options = {})
-      @client.put("domains/#{domain}/dkim_authority", options).to_h
-    end
-
-    # Public: Update the DKIM selector for a domains
-    #
-    # domain  - [String] Name of the domain (ex. domain.com)
-    # options - [Hash] of
-    #   dkim_selector  - [String] change the DKIM selector for a domain.
-    #
-    # Returns [Hash] with message key
-    def update_domain_dkim_selector(domain, options = {})
-      @client.put("domains/#{domain}/dkim_selector", options).to_h
-    end
-
-    # ==== End of Domain::Keys methods ====
 
     # ==== Domain::Tracking methods ====
 
@@ -346,16 +253,13 @@ module Mailgun
 
     # ==== End of Reporting::Stats methods ====
 
-    enforces_api_version 'v1', :list_domain_keys, :create_domain_key, :delete_domain_key, :dkim_rotation,
-                         :dkim_rotate
+    enforces_api_version 'v1', :dkim_rotation, :dkim_rotate
     enforces_api_version 'v2', :get_domain_tracking_certificate, :regenerate_domain_tracking_certificate,
                          :generate_domain_tracking_certificate
     enforces_api_version 'v3', :remove, :create_smtp_credentials, :update_smtp_credentials,
                          :delete_smtp_credentials, :get_domain_tracking_settings,
                          :update_domain_tracking_open_settings, :update_domain_tracking_click_settings,
-                         :update_domain_tracking_unsubscribe_settings, :update_domain_dkim_authority,
-                         :update_domain_dkim_selector, :get_domain_stats
-    enforces_api_version 'v4', :get_domain_keys, :activate_domain_key, :deactivate_domain_key,
-                         :list, :verify, :create, :update
+                         :update_domain_tracking_unsubscribe_settings, :get_domain_stats
+    enforces_api_version 'v4', :list, :verify, :create, :update
   end
 end
---
+++ spec/integration/domain_keys_spec.rb
@@ -0,0 +1,123 @@
+
+require 'spec_helper'
+require 'mailgun'
+
+vcr_opts = { cassette_name: 'domains' }
+
+describe 'DomainKeys', vcr: vcr_opts do
+  let(:api_version) { APIVERSION }
+  let(:mg_client) { Mailgun::Client.new(APIKEY, APIHOST, api_version, SSL) }
+  let(:mg_obj) { Mailgun::DomainKeys.new(mg_client) }
+  let(:domain) { 'integration-test.domain.invalid' }
+
+  describe '#list_domain_keys' do
+    let(:api_version) { 'v1' }
+
+    it 'lists all domain keys' do
+      result = mg_obj.list_domain_keys(
+        {
+          signing_domain: domain
+        }
+      )
+
+      expect(result).to include('items')
+      expect(result['items'].first['selector']).to eq('k1')
+    end
+  end
+
+  describe '#create_domain_key' do
+    let(:api_version) { 'v1' }
+
+    it 'creates a domain key' do
+      result = mg_obj.create_domain_key(
+        {
+          signing_domain: domain,
+          selector: 'test'
+        }
+      )
+
+      expect(result['signing_domain']).to eq(domain)
+      expect(result['selector']).to eq('test')
+    end
+  end
+
+  describe '#delete_domain_key' do
+    let(:api_version) { 'v1' }
+
+    it 'deletes a domain key' do
+      result = mg_obj.delete_domain_key(
+        {
+          signing_domain: domain,
+          selector: 'test'
+        }
+      )
+
+      expect(result['message']).to eq('success')
+    end
+  end
+
+  describe '#activate_domain_key' do
+    let(:api_version) { 'v4' }
+
+    it 'activates a domain key' do
+      result = mg_obj.activate_domain_key(
+        domain,
+        'mailo1'
+      )
+
+      expect(result['message']).to eq('domain key activated')
+    end
+  end
+
+  describe '#get_domain_keys' do
+    let(:api_version) { 'v4' }
+
+    it 'lists the domain keys for a specified signing domain' do
+      result = mg_obj.get_domain_keys(
+        domain
+      )
+
+      expect(result).to include('items')
+      expect(result['items'].first['selector']).to eq('mailo1')
+    end
+  end
+
+  describe '#deactivate_domain_key' do
+    let(:api_version) { 'v4' }
+
+    it 'deactivates a domain key' do
+      result = mg_obj.deactivate_domain_key(
+        domain,
+        'tetetet'
+      )
+
+      expect(result['message']).to eq('domain key deactivated')
+    end
+  end
+
+  describe '#update_domain_dkim_authority' do
+    it 'updates the DKIM authority for a domain' do
+      result = mg_obj.update_domain_dkim_authority(
+        domain,
+        {
+          self: true
+        }
+      )
+
+      expect(result['message']).to eq('Domain DKIM authority has not been changed')
+    end
+  end
+
+  describe '#update_domain_dkim_selector' do
+    it 'updates the DKIM selector for a domain' do
+      result = mg_obj.update_domain_dkim_selector(
+        domain,
+        {
+          dkim_selector: 'mailo1'
+        }
+      )
+
+      expect(result['message']).to eq('DKIM selector changed')
+    end
+  end
+end
